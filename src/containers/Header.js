b5e30ca0e5b196b920432139e5f3673193b899b6
nadeem gulzar <76985597+imnadeemgulzar@users.noreply.github.com>
2022-03-20 23:15:01 +0530

    final styling

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,10 +1,11 @@
+import { orange } from "@material-ui/core/colors";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="ui fixed menu">
-      <div className="ui container center">
-        <h2>The Online Kupo</h2>
+    <div className="ui fixed menu" style={{ backgroundColor: "#275" }}>
+      <div>
+        <h2 className="heading">The Online Kupo</h2>
       </div>
     </div>
   );
--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 import ProductComponent from "./ProductComponent";
 import { setProducts } from "../redux/actions/productActions";
 
 const ProductListing = () => {
   const products = useSelector((state) => state.allProducts.products);
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-evenly",
+      marginTop: "60px",
+      backgroundImage: "linear-gradient(to right,#133,#311)",
+    },
+  }));
   const dispatch = useDispatch();
   const fetchProducts = async () => {
     const response = await axios
@@ -18,8 +28,9 @@ const ProductListing = () => {
   useEffect(() => {
     fetchProducts();
   }, []);
+  const classes = useStyles();
   return (
-    <div className="ui grid container">
+    <div className={classes.container}>
       <ProductComponent />
     </div>
   );
